1f835ca9e26a822a3606b7600d78499d007aef21
Reful4ent <dima2323fr3@gmail.com>
2025-02-20 13:55:56 +0700

    feat: commands and BaseVM FOR MVVM

---
+++ ToFLaC/ViewModel/Commands/RelayCommands.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+namespace ToFLaC.ViewModel.Commands;
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T> _execute;
+
+    public RelayCommand(Action<T> execute)
+    {
+        _execute = execute;
+    }
+
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter)
+    {
+        if (parameter is T typedParameter)
+        {
+            _execute(typedParameter);
+        }
+    }
+
+    public event EventHandler CanExecuteChanged;
+}
