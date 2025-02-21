ae2bdc645f5e16793ca22a00c44aec071e6782df
Reful4ent <dima2323fr3@gmail.com>
2025-02-21 19:11:45 +0700

    feat: Дезигн и кнопки на вставить копировать и тд

--- ToFLaC/View/MainWindow.xaml.cs
+++ ToFLaC/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using ToFLaC.ViewModel;
 
 namespace ToFLaC.View;
 
@@ -7,5 +8,21 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainVM();
+    }
+
+    private void ButtonCopy_OnClick(object sender, RoutedEventArgs e)
+    {
+        codeBox.Copy();
+    }
+
+    private void ButtonCut_OnClick(object sender, RoutedEventArgs e)
+    {
+        codeBox.Cut();
+    }
+
+    private void ButtonInsert_OnClick(object sender, RoutedEventArgs e)
+    {
+        codeBox.Paste();
     }
 }
--- ToFLaC/ViewModel/MainVM.cs
+++ ToFLaC/ViewModel/MainVM.cs
@@ -2,5 +2,12 @@ namespace ToFLaC.ViewModel;
 
 public class MainVM : BaseVM
 {
-    
+    string enteredCode = string.Empty;
+
+    public string EnteredCode
+    {
+        get => enteredCode;
+        set => Set(ref enteredCode, value);
+
+    }
 }
